f7bebe2e3248ca58c57f7a27a657b05078fb6e53
corcor27 <cory13579@googlemail.com>
2019-10-10 13:58:22 +0100

    Update converion.sh

--- converion.sh
+++ converion.sh
@@ -1,5 +1,5 @@
 from PIL import Image
 img = Image.open("boob-test1.jpg")
-area = (400, 400, 800, 800)
+area = (2388, 2540, 586, 985)
 cropped_img = img.crop(area)
 cropped_img.save("Run22_lal_chi_p.png")
