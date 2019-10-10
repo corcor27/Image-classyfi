4e0d21a74e3792fa12ee16433c52551d5340d93e
corcor27 <cory13579@googlemail.com>
2019-10-10 13:41:46 +0100

    Update converion.sh

--- converion.sh
+++ converion.sh
@@ -1,19 +1,5 @@
 from PIL import Image
-
- 
-
-# Create an Image object from an Image
-
-imageObject  = Image.open(/home/cot12/Documents/image_10-10-2019_0.jpg)
-
- 
-
-# Crop the iceberg portion
-
-cropped     = imageObject.crop((2388,2540,586,895))
-
- 
-
-# Display the cropped portion
-
-cropped.show()
+img = Image.open("boob-test1.jpg")
+area = (400, 400, 800, 800)
+cropped_img = img.crop(area)
+cropped_img.show()
