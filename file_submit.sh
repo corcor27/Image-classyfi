5d53a88a3ac013fb11e97c703a31b59172f0872e
corcor27 <cory13579@googlemail.com>
2019-11-01 11:57:50 +0000

    Update file_submit.sh

--- file_submit.sh
+++ file_submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo Your environment is ready good sir \
 
-sbatch --gres gpu:1 anaconda3-launch -env coryenv python creating-SIFT.py
+sbatch --gres gpu:1 anaconda3-launch -env corysift python creating-SIFT.py
 
