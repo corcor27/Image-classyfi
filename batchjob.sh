b2758ee56b62354ae0b96e47d4ede0f94bb2780c
corcor27 <cory13579@googlemail.com>
2019-11-19 18:51:58 +0000

    Create batchjob.sh

---
+++ batchjob.sh
@@ -0,0 +1,24 @@
+#!/bin/bash --login
+###
+# job name
+#SBATCH --job-name=hostname
+# job stdout file
+#SBATCH --output=hostname.out.%J
+# job stderr file
+#SBATCH --error=hostname.err.%J
+# maximum job time in D-HH:MM
+#SBATCH --time=0-00:01
+# maximum memory of 10 megabytes
+#SBATCH --mem-per-cpu=10
+# run a single task, using a single CPU core
+#SBATCH --ntasks=1
+# specify our current project
+# change this for your own work
+#SBATCH --account=scwXXXX
+# specify the reservation we have for the training workshop
+# replace XX with the code provided by your instructor
+# remove this for your own work
+#SBATCH --reservation=scwXXXX_YY
+###
+
+/bin/hostname
