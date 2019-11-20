a64f9ab40f85957865a5f6c65ae73ea0252e37a8
corcor27 <cory13579@googlemail.com>
2019-11-20 14:23:37 +0000

    Update batchjob.sh

--- batchjob.sh
+++ batchjob.sh
@@ -1,27 +1,20 @@
-#!/bin/bash --login
-###
-# job name
-#SBATCH --job-name=hostname
-# job stdout file
-#SBATCH --output=hostname.out.%J
-# job stderr file
-#SBATCH --error=hostname.err.%J
-# maximum job time in D-HH:MM
-#SBATCH --time=0-00:01
-# maximum memory of 10 megabytes
-#SBATCH --mem-per-cpu=10
-# run a single task, using a single CPU core
-#SBATCH --ntasks=1
+#!/bin/bash
+
+
+###                                                                                                                                                                                       
+#SBATCH --job-name=py_mpi_hostname
+#SBATCH --output=py_mpi_hostname.out.%J.%N
+#SBATCH --error=py_mpi_hostname.err.%J.%N
 # specify our current project
-# change this for your own work
-#SBATCH --account=scw1389
+# replace XXXX with the code provided by your instructor
+#SBATCH --account=scwXXXX
 # specify the reservation we have for the training workshop
-# replace XX with the code provided by your instructor
 # remove this for your own work
+# replace XXXX and YY with the code provided by your instructor
+#SBATCH --reservation=scwXXXX_YY
 
-#SBATCH --reservation=scw1389_23
-#SBATCH --mail-user=cot12@aber.ac.uk
-#SBATCH --mail-type=ALL
 ###
 
-/bin/hostname
+module load mpi
+module load python/3.7.0
+mpirun python3 print_hostname.py
