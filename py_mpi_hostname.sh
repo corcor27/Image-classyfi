4a755dc8db72e795b1bfade39462030c8458d67e
corcor27 <cory13579@googlemail.com>
2019-11-20 14:39:46 +0000

    Update py_mpi_hostname.sh

--- py_mpi_hostname.sh
+++ py_mpi_hostname.sh
@@ -1,20 +1,10 @@
-#!/bin/bash
-
-
-###                                                                                                                                                                                       
-#SBATCH --job-name=py_mpi_hostname
-#SBATCH --output=py_mpi_hostname.out.%J.%N
-#SBATCH --error=py_mpi_hostname.err.%J.%N
-# specify our current project
-# replace XXXX with the code provided by your instructor
-#SBATCH --account=scw1389
-# specify the reservation we have for the training workshop
-# remove this for your own work
-# replace XXXX and YY with the code provided by your instructor
-#SBATCH --reservation=scw1389_23
-
+#!/bin/sh
 ###
-
+#SBATCH --job-name=mpi_date
+#SBATCH --output=mpi_date.out.%J.%N
+#SBATCH --error=mpi_date.err.%J.%N
+#SBATCH --account=scw1389
+#SBATCH --reservation=scw1389_23
+###
 module load mpi
-module load python/3.7.0
-mpirun python3 print_hostname.py
+mpirun date +%M:%S.%N
